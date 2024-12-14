c5c2fddeec2058ef822dc1fb4b1fbf4994799908
Rahgul Krishna C <rahgulkrishnac@gmail.com>
2024-12-14 15:05:29 +0530

    Day 1 Commt

---
+++ day1/identifierdemo.java
@@ -0,0 +1,13 @@
+package com.tns.ifet.day1;
+
+public class identifierdemo {
+
+	public static void main(String[] args) {
+		int $number=20;
+		System.out.println("value of the number variable is :"+ $number);
+		String studentName="RahgulKrishna";
+		System.out.println("value is :"+ studentName);
+
+	}
+
+}
