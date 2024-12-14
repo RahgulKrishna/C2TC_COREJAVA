d7f5b8f810daab7c7af031d949cbde2c9e2a8770
Rahgul Krishna C <rahgulkrishnac@gmail.com>
2024-12-14 15:07:49 +0530

    Day 5 Commit

---
+++ day5/singleinheritance/SLInheritanceDemo.java
@@ -0,0 +1,10 @@
+package com.tns.ifet.day5.singleinheritance;
+
+public class SLInheritanceDemo 
+{
+	public static void main(String[] args) 
+	{
+		Student student = new Student("Rahgul_Krishna", "8080808080", "Bangalore", 9888878787L, 10, "CityCollege"); 
+		System.out.println(student); 
+	}
+}
